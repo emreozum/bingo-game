e4f777b59a825cd698aedede819fb2e0d66ff539
emreozum <h.emreozum@gmail.com>
2023-02-20 22:39:24 +0300

    Add: Feature
    Added one row, two rows or bingo state
    
    Added reset card button

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
 
   const [cardArray, setCardArray] = useState(generateCardArray())
   const [bingoNumber, setBingoNumber] = useState(0)
-  const [randomElement, setRandomElement] = useState(0)
+  const [rowState, setRowState] = useState('')
 
   function generateNumber() {
     return{
-     value: Math.floor(Math.random() * 90+1),
+     value: Math.floor(Math.random() * 90),
      id: nanoid(),
      isHeld: false
     } 
@@ -27,7 +27,37 @@ function App() {
     return cardArray
   }
 
-  
+  //if one of any row is held, then log 'One Row!'
+  //if two of any rows are held, then log 'Two Rows!'
+  //if three rows are all held, then log 'Bingo'
+  function CheckRowState(){
+    const row1 = cardArray.slice(0, 9)
+    const row2 = cardArray.slice(9, 18)
+    const row3 = cardArray.slice(18, 27)
+
+    const row1Held = row1.every(card => card.isHeld)
+    const row2Held = row2.every(card => card.isHeld)
+    const row3Held = row3.every(card => card.isHeld)
+
+    if (row1Held || row2Held || row3Held){
+      setRowState('One Row!')
+      if (row1Held && row2Held || row1Held && row3Held || row2Held && row3Held){
+        setRowState('Two Rows!')
+        if (row1Held && row2Held && row3Held){
+          setRowState('Bingo!')
+        }
+      }
+    }
+    else{
+      setRowState('Keep Going!')
+    }
+  }
+
+  //reset card array
+  function resetCardArray(){
+    setCardArray(generateCardArray())
+  }
+
   function handleClick(id){
     setCardArray(oldCardArray => oldCardArray.map(card => {
       if (card.id === id) {
@@ -42,7 +72,7 @@ function App() {
   }
 
   function generateBingoNumber(){
-    setBingoNumber(Math.floor(Math.random() * 99))
+    setBingoNumber(Math.floor(Math.random() * 90))
     setCardArray(oldCardArray => oldCardArray.map(card => {
       if (card.value === bingoNumber) {
         return {
@@ -67,16 +97,23 @@ function App() {
 
   return (
     <main>
+      <CheckRowState />
       <h1 className='title'>
        Welcome to the Bingo Game!
       </h1>
         <div className='card-array' onClick={handleClick}>
             {cardElements}
         </div>
+        <div>
+          <h2 className='row-state'>{rowState}</h2>
+        </div>
         
         <div>
           <button className='button' onClick={generateBingoNumber}>Generate Bingo Number</button>
           
+        </div>
+        <div>
+        <button className='button' onClick={resetCardArray}>Reset Card</button>
         </div>
         <div>
           <h2 className='bingo-number'>Bingo Number: {bingoNumber}</h2>
